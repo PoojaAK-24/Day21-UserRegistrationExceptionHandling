afd593372f69b4f9aef856440d220ed01872bdff
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-28 18:18:34 +0530

    Pooja Added:Validate First Name Using Exception Handling

--- Day21-UserRegistrationExceptionHandling/Program.cs
+++ Day21-UserRegistrationExceptionHandling/Program.cs
@@ -6,7 +6,7 @@ namespace Day21_UserRegistrationExceptionHandling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration Exception Problem!");
+            Console.WriteLine("*Welcome to User Registration Exception Problem*");
             Patterns patternsMatch = new Patterns();
             Console.WriteLine("Enter FirstName:");
             string firstName = Console.ReadLine();
